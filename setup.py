c073b2ddf01250f3fc9062b21597c0f42b4c45ff
Mark Parker <msparker@sky.com>
2021-01-09 19:10:53 +0000

    chg setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-from distutils.core import setup
-setup(
-  name = 'wiserHeatAPIv2',
-  packages = ['wiserHeatAPIv2'],
-  version = '0.0.1',
-  license='MIT',
-  description = 'A simple API for controlling the Drayton Wiser Heating system',
-  author = 'Mark Parker',
-  author_email = 'msparker@sky.com',
-  url = 'https://github.com/msp1974/wiserheatapiv2',
-  download_url = 'https://github.com/msp1974/wiserheatapiv2/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Wiser', 'Heating', 'V2', 'API'],
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
-  ],
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wiserHeatAPIv2", # Replace with your own username
+    version="0.0.1",
+    author="Mark Parker",
+    author_email="msparker@sky.com",
+    description="An API for controlling the Drayton Wiser Heating system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msp1974/wiserheatapiv2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
