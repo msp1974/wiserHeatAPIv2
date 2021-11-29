bb8521687a2f8c48b776b7d5d1792adf6c8d90e0
Mark Parker <msparker@sky.com>
2021-11-29 17:35:56 +0000

    move battery class to device

--- wiserHeatAPIv2/device.py
+++ wiserHeatAPIv2/device.py
@@ -1,4 +1,10 @@
-from .const import TEXT_UNKNOWN
+from .const import (
+    TEXT_UNKNOWN,
+    ROOMSTAT_MIN_BATTERY_LEVEL,
+    ROOMSTAT_FULL_BATTERY_LEVEL,
+    TRV_MIN_BATTERY_LEVEL,
+    TRV_FULL_BATTERY_LEVEL,
+)
 
 class _WiserDevice(object):
     """Class representing a wiser device"""
@@ -46,6 +52,50 @@ class _WiserDevice(object):
     def signal(self) -> object:
         """Get zwave network information"""
         return self._signal
+
+class _WiserBattery(object):
+    """Data structure for battery information for a Wiser device that is powered by batteries"""
+
+    def __init__(self, data: dict):
+        self._data = data
+
+    @property
+    def level(self) -> str:
+        """Get the descritpion of the battery level"""
+        return self._data.get("BatteryLevel", "No Battery")
+
+    @property
+    def percent(self) -> int:
+        """Get the percent of battery remaining"""
+        if self._data.get("ProductType") == "RoomStat" and self.level != "No Battery":
+            return min(
+                100,
+                round(
+                    (
+                        (self.voltage - ROOMSTAT_MIN_BATTERY_LEVEL)
+                        / (ROOMSTAT_FULL_BATTERY_LEVEL - ROOMSTAT_MIN_BATTERY_LEVEL)
+                    )
+                    * 100
+                ),
+            )
+        elif self._data.get("ProductType") == "iTRV" and self.level != "No Battery":
+            return min(
+                100,
+                round(
+                    (
+                        (self.voltage - TRV_MIN_BATTERY_LEVEL)
+                        / (TRV_FULL_BATTERY_LEVEL - TRV_MIN_BATTERY_LEVEL)
+                    )
+                    * 100
+                ),
+            )
+        else:
+            return 0
+
+    @property
+    def voltage(self) -> float:
+        """Get the battery voltage"""
+        return self._data.get("BatteryVoltage", 0) / 10
 
 
 
