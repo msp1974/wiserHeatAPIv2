899bd56dc4bb937b7b41882b38b688660a5b5b24
Mark Parker <msparker@sky.com>
2021-11-29 17:36:10 +0000

    move battery class to device

--- wiserHeatAPIv2/helpers.py
+++ wiserHeatAPIv2/helpers.py
@@ -1,9 +1,5 @@
 from . import _LOGGER
 from .const import (
-    ROOMSTAT_MIN_BATTERY_LEVEL,
-    ROOMSTAT_FULL_BATTERY_LEVEL,
-    TRV_MIN_BATTERY_LEVEL,
-    TRV_FULL_BATTERY_LEVEL,
     TEMP_ERROR,
     TEMP_MINIMUM,
     TEMP_MAXIMUM,
@@ -13,52 +9,6 @@ from .const import (
     MAX_BOOST_INCREASE,
     WiserUnitsEnum,
 )
-
-
-class _WiserBattery(object):
-    """Data structure for battery information for a Wiser device that is powered by batteries"""
-
-    def __init__(self, data: dict):
-        self._data = data
-
-    @property
-    def level(self) -> str:
-        """Get the descritpion of the battery level"""
-        return self._data.get("BatteryLevel", "No Battery")
-
-    @property
-    def percent(self) -> int:
-        """Get the percent of battery remaining"""
-        if self._data.get("ProductType") == "RoomStat" and self.level != "No Battery":
-            return min(
-                100,
-                round(
-                    (
-                        (self.voltage - ROOMSTAT_MIN_BATTERY_LEVEL)
-                        / (ROOMSTAT_FULL_BATTERY_LEVEL - ROOMSTAT_MIN_BATTERY_LEVEL)
-                    )
-                    * 100
-                ),
-            )
-        elif self._data.get("ProductType") == "iTRV" and self.level != "No Battery":
-            return min(
-                100,
-                round(
-                    (
-                        (self.voltage - TRV_MIN_BATTERY_LEVEL)
-                        / (TRV_FULL_BATTERY_LEVEL - TRV_MIN_BATTERY_LEVEL)
-                    )
-                    * 100
-                ),
-            )
-        else:
-            return 0
-
-    @property
-    def voltage(self) -> float:
-        """Get the battery voltage"""
-        return self._data.get("BatteryVoltage", 0) / 10
-
 
 class _WiserTemperatureFunctions(object):
     # -----------------------------------------------------------
