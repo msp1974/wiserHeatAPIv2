6bbcd586de5a089d75d9f678e03cdca5f7630bc1
Mark Parker <msparker@sky.com>
2022-03-09 11:18:50 +0000

    improve capabilities property

--- wiserHeatAPIv2/helpers.py
+++ wiserHeatAPIv2/helpers.py
@@ -424,20 +424,54 @@ class _WiserHubCapabilitiesInfo:
         self._data = data
 
     @property
-    def all(self) -> str:
+    def all(self) -> dict:
         "Get the list of capabilities"
-        return self._data
+        return dict(self._data)
+
+    @property
+    def smartplug(self):
+        return self._data.get("SmartPlug", False)
+
+    @property
+    def itrv(self):
+        return self._data.get("ITRV", False)
+
+    @property
+    def roomstat(self):
+        return self._data.get("Roomstat", False)
+
+    @property
+    def ufh(self):
+        return self._data.get("UFH", False)
+
+    @property
+    def ufh_floor_temp_sensor(self):
+        return self._data.get("UFHFloorTempSensor", False)
+
+    @property
+    def ufh_dew_sensor(self):
+        return self._data.get("UFHDewSensor", False)
+
+    @property
+    def hact(self):
+        return self._data.get("HACT", False)
+
+    @property
+    def lact(self):
+        return self._data.get("LACT", False)
+
+    @property
+    def light(self):
+        return self._data.get("Light", False)
+
+    @property
+    def shutter(self):
+        return self._data.get("Shutter", False)
+
+    @property
+    def load_controller(self):
+        return self._data.get("LoadController", False)
     
-    def get_capability(self, name) -> bool:
-        """
-        Gets a capability value from the hub capabilities
-        param name: name of capability
-        return: bool
-        """
-        try:
-            return [capability.value for capability in self.capabilities if capability.key == name][0]
-        except IndexError:
-            return None
 
 class _WiserOutputRange(object):
     """ Data structure for min/max output range"""
