46ac61871a5fe245ba8e12d9c23374d5c2cf46ee
Mark Parker <msparker@sky.com>
2022-05-06 18:30:16 +0100

    add opentherm endpoint and class to system

--- wiserHeatAPIv2/helpers.py
+++ wiserHeatAPIv2/helpers.py
@@ -360,6 +360,33 @@ class _WiserCloud:
         """Get if diagnostic telemetry is enabled"""
         return self._data.get("EnableDiagnosticTelemetry", False)
 
+class _WiserOpentherm:
+    """Data structure for Opentherm data"""
+    def __init__(self, data: dict, enabled_status: str):
+        self._data = data
+        self._enabled_status = enabled_status
+        self._operational_data = data.get("operationalData", {})
+
+    @property
+    def ch_pressure_bar(self) -> str:
+        """Get ChPressureBar operationalData"""
+        return self._operational_data.get("ChPressureBar", TEXT_UNKNOWN)
+
+    @property
+    def ch_flow_temperature(self) -> str:
+        """Get Ch1FlowTemperature operationalData"""
+        return self._operational_data.get("Ch1FlowTemperature", TEXT_UNKNOWN)
+
+    @property
+    def ch_return_temperature(self) -> str:
+        """Get ChReturnTemperature operationalData"""
+        return self._operational_data.get("ChReturnTemperature", TEXT_UNKNOWN)
+
+    @property
+    def connection_status(self) -> str:
+        """Get opentherm connection status"""
+        return self._enabled_status
+
 
 class _WiserZigbee:
     """Data structure for zigbee information for a Wiser Hub"""
